cc51c5c7aad035143743d07e8443f7abe6c56ef3
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-03 22:47:43 +0000

    Implement HotelSearchService ReturnsListWithOneEntry unit test

--- tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
+++ tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 public class HotelSearchServiceTests
 {
     [Fact]
-    public void FlightSearch_ValidFlightDataListWithNoMatch_ReturnsEmptyList()
+    public void HotelSearch_ValidHotelDataListWithNoMatch_ReturnsEmptyList()
     {
         List<Hotel> hotels = new List<Hotel> {
             new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
@@ -21,25 +21,34 @@ public class HotelSearchServiceTests
     }
 
     [Fact]
-    public void FlightSearch_ValidFlightDataList_ReturnsListWithOneEntry()
+    public void HotelSearch_ValidHotelDataList_ReturnsListWithOneEntry()
     {
-        
+        List<Hotel> hotels = new List<Hotel> {
+            new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
+            new Hotel{ arrival_date = "2022-11-05", id = 2, local_airports = ["TFS"], name = "Laguna Park 2", nights = 7, price_per_night = 50},
+            new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 14, price_per_night = 59},
+        };
+
+        var hotel_service = new HotelSearchService(hotel_data: hotels);
+        List<Hotel> searched_list = hotel_service.HotelSearch(to: "PMI", date: "2023-06-15");
+        Assert.Single(searched_list);
+        Assert.Equal(expected: 3, actual: searched_list[0].id);
     }
 
     [Fact]
-    public void FlightSearch_ValidFlightDataList_ReturnsListWithMultipleEntries()
+    public void HotelSearch_ValidHotelDataList_ReturnsListWithMultipleEntries()
     {
         Assert.True(false);
     }
 
     [Fact]
-    public void FindCheapestFlight_ValidFlightData_ReturnsCheapestFlight()
+    public void FindCheapestHotel_ValidHotelData_ReturnsCheapestHotel()
     {
         Assert.True(false);
     }
 
     [Fact]
-    public void FindCheapestFlight_EmptyFlightData_ThrowsArgumentException()
+    public void FindCheapestHotel_EmptyHotelData_ThrowsArgumentException()
     {
         Assert.True(false);
     }
