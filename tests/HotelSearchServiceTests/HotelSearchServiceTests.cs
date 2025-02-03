947288a7daad5994f561045e0d02657ebbdaf503
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-03 22:54:06 +0000

    Implement HotelSearchService ReturnsListWithMultipleEntries test

--- tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
+++ tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
@@ -38,7 +38,17 @@ public class HotelSearchServiceTests
     [Fact]
     public void HotelSearch_ValidHotelDataList_ReturnsListWithMultipleEntries()
     {
-        Assert.True(false);
+        List<Hotel> hotels = new List<Hotel> {
+            new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
+            new Hotel{ arrival_date = "2023-06-15", id = 2, local_airports = ["PMI"], name = "Laguna Park 2", nights = 7, price_per_night = 50},
+            new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 14, price_per_night = 59},
+        };
+
+        var hotel_service = new HotelSearchService(hotel_data: hotels);
+        List<Hotel> searched_list = hotel_service.HotelSearch(to: "PMI", date: "2023-06-15");
+        Assert.Equal(expected: 2, actual: searched_list.Count());
+        Assert.Equal(expected: 2, actual: searched_list[0].id);
+        Assert.Equal(expected: 3, actual: searched_list[1].id);
     }
 
     [Fact]
