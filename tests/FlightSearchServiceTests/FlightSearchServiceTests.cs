e9ab7d466a339315668cd996610c1ad2971e51e0
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-04 13:35:57 +0000

    Refactor remaining FlightSearchService unit tests

--- tests/FlightSearchServiceTests/FlightSearchServiceTests.cs
+++ tests/FlightSearchServiceTests/FlightSearchServiceTests.cs
@@ -40,7 +40,7 @@ public class FlightSearchServiceTests
         Assert.Single(collection: filtered_result);
         Assert.Equal(expected: flights[0].id, actual: filtered_result[0].id);
     }
-/*
+
     [Fact]
     public void FlightSearch_ValidFlightDataList_ReturnsListWithMultipleEntries()
     {
@@ -51,24 +51,32 @@ public class FlightSearchServiceTests
             new Flight { airline = "Southern Airlines", departure_date = "2023-07-01", from = "MAN", id = 4, price = 170, to = "TFS" }
         };
 
-        var flight_service = new FlightSearchService(flights);
-        List<Flight> searched_list = flight_service.FlightSearch(to: "TFS", from: "MAN", date: "2023-07-01");
-        Assert.Equal(expected: 2, actual: searched_list.Count());
-        Assert.Equal(expected: flights[0].id, actual: searched_list[0].id);
-        Assert.Equal(expected: flights[3].id, actual: searched_list[1].id);
+        var flight_service = new FlightSearchService(
+            flight_data: flights,
+            user_search: new UserSearch{to = "TFS", from = "MAN", date = "2023-07-01"}
+        );
+        List<Flight> filtered_result = flight_service.Filter();
+
+        Assert.Equal(expected: 2, actual: filtered_result.Count());
+        Assert.Equal(expected: flights[0].id, actual: filtered_result[0].id);
+        Assert.Equal(expected: flights[3].id, actual: filtered_result[1].id);
     }
+
 
     [Fact]
     public void FindCheapestFlight_ValidFlightData_ReturnsCheapestFlight()
     {
         List<Flight> flights = new List<Flight> {
             new Flight { airline = "First Class Air", departure_date = "2023-07-01", from = "MAN", id = 1, price = 470, to = "TFS" },
-            new Flight { airline = "Oceanic Airlines", departure_date = "2023-07-01", from = "MAN", id = 2, price = 245, to = "AGP" },
-            new Flight { airline = "Trans American Airlines", departure_date = "2023-06-15", from = "MAN", id = 3, price = 170, to = "PMI" }
+            new Flight { airline = "Oceanic Airlines", departure_date = "2023-07-01", from = "MAN", id = 2, price = 245, to = "TFS" },
+            new Flight { airline = "Trans American Airlines", departure_date = "2023-07-01", from = "MAN", id = 3, price = 170, to = "TFS" }
         };
 
-        var flight_service = new FlightSearchService(flights);
-        Flight cheapest = flight_service.FindCheapestFlight(flights: flights);
+        var flight_service = new FlightSearchService(
+            flight_data: flights,
+            user_search: new UserSearch{to = "TFS", from = "MAN", date = "2023-07-01"}
+        );
+        Flight cheapest = flight_service.FindCheapestFlight();
         Assert.Equal(flights[2].id, cheapest.id);
     }
 
@@ -77,8 +85,11 @@ public class FlightSearchServiceTests
     {
         List<Flight> flights = new List<Flight> {};
 
-        var flight_service = new FlightSearchService(flights);
-        Assert.Throws<ArgumentException>(() => flight_service.FindCheapestFlight(flights));
+        var flight_service = new FlightSearchService(
+            flight_data: flights,
+            user_search: new UserSearch{to = "TFS", from = "MAN", date = "2023-07-01"}
+        );
+        Assert.Throws<ArgumentException>(() => flight_service.FindCheapestFlight());
     } 
-    */
+    
 }
