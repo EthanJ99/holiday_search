ce2ed2de8345dc37977227af588a9bfa318525c1
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-02-04 17:10:31 +0000

    Finish refactoring HotelSearchService tests

--- tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
+++ tests/HotelSearchServiceTests/HotelSearchServiceTests.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 public class HotelSearchServiceTests
 {
     [Fact]
-    public void HotelSearch_ValidHotelDataListWithNoMatch_ReturnsEmptyList()
+    public void Filter_ValidHotelDataListWithNoMatch_ReturnsEmptyList()
     {
         List<Hotel> hotels = new List<Hotel> {
             new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
@@ -16,42 +16,49 @@ public class HotelSearchServiceTests
 
         var hotel_service = new HotelSearchService(
             hotel_data: hotels,
-            user_search: new UserSearch{to = "MAN", date = "2022-01-01", duration = 7});
+            user_search: new UserSearch{to = "MAN", date = "2022-01-01", duration = 7}
+        );
         List<Hotel> filtered_list = hotel_service.Filter();
         Assert.Empty(filtered_list);
     
     }
-/*
+
     [Fact]
-    public void HotelSearch_ValidHotelDataList_ReturnsListWithOneEntry()
+    public void Filter_ValidHotelDataList_ReturnsListWithOneEntry()
     {
         List<Hotel> hotels = new List<Hotel> {
             new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
-            new Hotel{ arrival_date = "2022-11-05", id = 2, local_airports = ["TFS"], name = "Laguna Park 2", nights = 7, price_per_night = 50},
+            new Hotel{ arrival_date = "2022-11-05", id = 2, local_airports = ["TPR"], name = "Laguna Park 2", nights = 7, price_per_night = 50},
             new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 14, price_per_night = 59},
         };
 
-        var hotel_service = new HotelSearchService(hotel_data: hotels);
-        List<Hotel> searched_list = hotel_service.HotelSearch(to: "PMI", date: "2023-06-15");
-        Assert.Single(searched_list);
-        Assert.Equal(expected: 3, actual: searched_list[0].id);
-    }
+        var hotel_service = new HotelSearchService(
+            hotel_data: hotels,
+            user_search: new UserSearch{to = "TFS", date = "2022-11-05", duration = 7}
+        );
+        List<Hotel> filtered_list = hotel_service.Filter();
+        Assert.Single(filtered_list);
+        Assert.Equal(expected: 1, actual: filtered_list[0].id);
+    } 
 
     [Fact]
-    public void HotelSearch_ValidHotelDataList_ReturnsListWithMultipleEntries()
+    public void Filter_ValidHotelDataList_ReturnsListWithMultipleEntries()
     {
         List<Hotel> hotels = new List<Hotel> {
             new Hotel{ arrival_date = "2022-11-05", id = 1, local_airports = ["TFS"], name = "Iberostar Grand Portals Nous", nights = 7, price_per_night = 100},
             new Hotel{ arrival_date = "2023-06-15", id = 2, local_airports = ["PMI"], name = "Laguna Park 2", nights = 7, price_per_night = 50},
-            new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 14, price_per_night = 59},
+            new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 7, price_per_night = 59},
         };
 
-        var hotel_service = new HotelSearchService(hotel_data: hotels);
-        List<Hotel> searched_list = hotel_service.HotelSearch(to: "PMI", date: "2023-06-15");
-        Assert.Equal(expected: 2, actual: searched_list.Count());
-        Assert.Equal(expected: 2, actual: searched_list[0].id);
-        Assert.Equal(expected: 3, actual: searched_list[1].id);
-    }
+        var hotel_service = new HotelSearchService(
+            hotel_data: hotels,
+            user_search: new UserSearch{to = "PMI", date = "2023-06-15", duration = 7}
+        );
+        List<Hotel> filtered_list = hotel_service.Filter();
+        Assert.Equal(expected: 2, actual: filtered_list.Count());
+        Assert.Equal(expected: 2, actual: filtered_list[0].id);
+        Assert.Equal(expected: 3, actual: filtered_list[1].id);
+    } 
 
     [Fact]
     public void FindCheapestHotel_ValidHotelData_ReturnsCheapestHotel()
@@ -62,16 +69,22 @@ public class HotelSearchServiceTests
             new Hotel{ arrival_date = "2023-06-15", id = 3, local_airports = ["PMI"], name = "Sol Katmandu Park & Resort", nights = 14, price_per_night = 59},
         };
 
-        var hotel_service = new HotelSearchService(hotels);
-        Hotel cheapest = hotel_service.FindCheapestHotel(hotels: hotels, nights: 7);
+        var hotel_service = new HotelSearchService(
+            hotel_data: hotels,
+            user_search: new UserSearch{to = "PMI", date = "2023-06-15", duration = 7}
+        );
+        Hotel cheapest = hotel_service.FindCheapestHotel();
         Assert.Equal(hotels[1].id, cheapest.id);
-    }
+    } 
 
     [Fact]
     public void FindCheapestHotel_EmptyHotelData_ThrowsArgumentException()
     {
         List<Hotel> hotels = new List<Hotel> {};
-        var hotel_service = new HotelSearchService(hotels);
-        Assert.Throws<ArgumentException>(() => hotel_service.FindCheapestHotel(hotels: hotels, nights: 7));
-    } */
+        var hotel_service = new HotelSearchService(
+            hotel_data: hotels,
+            user_search: new UserSearch{to = "PMI", date = "2023-06-15", duration = 7}
+        );
+        Assert.Throws<ArgumentException>(() => hotel_service.FindCheapestHotel());
+    } 
 }
